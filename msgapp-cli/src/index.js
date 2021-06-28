cc311404046f562306275dcd169a8c93bfbc2c6d
Dejan Nadlacki <Dejan.Nadlacki@netcetera.com>
2021-06-28 14:04:02 +0200

    Move css code to index

--- msgapp-cli/src/index.js
+++ msgapp-cli/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './resource/css/index.css';
 import App from './components/app/App';
 import reportWebVitals from './reportWebVitals';
+import 'antd/dist/antd.css';
 
 ReactDOM.render(
   <React.StrictMode>
