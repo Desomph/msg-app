8686596c6246b25800a8b9fc164318a6b6938a50
Dejan Nadlacki <Dejan.Nadlacki@netcetera.com>
2021-06-28 13:57:24 +0200

    Add index.js file with the logic handling communication

---
+++ msgapp-srv/index.js
@@ -0,0 +1,44 @@
+const WebSocket = require('ws');
+
+require('dotenv').config();
+
+// Server configuration.
+const port = process.env.SERVER_PORT || 8080;
+const webSocketServer = new WebSocket.Server({
+  port: port
+});
+
+webSocketServer.on('connection', (websocket) => {
+  websocket.send('Welcome to the messenger!');
+
+  websocket.on('message', (data) => {
+    let message;
+
+    try {
+      message = JSON.parse(data);
+    } catch (e) {
+      sendError(websocket, 'Wrong format');
+
+      return;
+    }
+
+    if (message.type === 'NEW_MESSAGE') {
+      webSocketServer.clients.forEach((client) => {
+        /// === 1 Is 'OPEN' - for some reason it's not exporting an enum.
+        if (client !== websocket && client.readyState === 1) {
+          client.send(data);
+        }
+      });
+    }
+
+  });
+});
+
+const sendError = (ws, message) => {
+  const error = {
+    type: 'ERROR',
+    payload: message,
+  };
+
+  ws.send(JSON.stringify(error));
+};
